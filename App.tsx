f1a31270cad7d79e3367f1afdd107de795350d50
hoangducbinh <hoangbinh02022002@gmail.com>
2024-02-18 22:05:42 +0700

    first

---
+++ App.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View} from "react-native"
+
+const App = () => {
+  return(
+      <View style={styles.container}>
+            <Text>
+
+            </Text>
+            <View>
+            </View>
+      </View>
+  )
+}
+const styles = StyleSheet.create({
+
+  container :{
+    flex :1
+  }
+})
+
+export default App;
